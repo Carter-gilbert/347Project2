69755345125bdfde9ae6f56857b8cbcf0958c41b
Carter Gilbert <GILBE3AC@dukes.jmu.edu>
2021-10-24 18:25:39 -0400

    added service

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const service = express();
 const fs = require('fs');
 const mysql = require('mysql');
 
 const json = fs.readFileSync('credentials.json', 'utf8');
 const credentials = JSON.parse(json);
-
 const connection = mysql.createConnection(credentials);
+
+const service = express();
+
 connection.connect(error => {
     if (error) {
         console.error(error);
@@ -15,10 +16,10 @@ connection.connect(error => {
 });
 
 
-// const port = 8443;
-// service.listen(port, () => {
-//     console.log(`We're live on port ${port}!`);
-// });
+const port = 8443;
+service.listen(port, () => {
+    console.log(`We're live on port ${port}!`);
+});
 
 
 const selectQuery = 'SELECT * FROM project2';
@@ -30,15 +31,15 @@ connection.query(selectQuery, (error, rows) => {
     }
 });
 
-const insertQuery = 'INSERT INTO project2(question, answer1, answer2, answer3, answer4, correct_ans) VALUES (?, ?, ?, ?, ?, ?)';
-const parameters = ["What is 2 + 2", "1", "2", "3", "4", 4];
-connection.query(insertQuery, parameters, (error, result) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(result);
-    }
-});
+// const insertQuery = 'INSERT INTO project2(question, answer1, answer2, answer3, answer4, correct_ans) VALUES (?, ?, ?, ?, ?, ?)';
+// const parameters = ["What is 2 + 2", "1", "2", "3", "4", 4];
+// connection.query(insertQuery, parameters, (error, result) => {
+//     if (error) {
+//         console.error(error);
+//     } else {
+//         console.log(result);
+//     }
+// });
 
 function rowToQuestion(row) {
     return {
@@ -51,7 +52,7 @@ function rowToQuestion(row) {
         correct_ans: row.correct_ans,
     };
 }
-
+// this just gets the json of the vars from rowToQuestion so question, the ans, and correct_ans
 connection.query('SELECT * FROM project2', (error, rows) => {
     if (error) {
         console.error(error);
@@ -61,6 +62,28 @@ connection.query('SELECT * FROM project2', (error, rows) => {
     }
 });
 
+service.get('/questions/:id', (request, response) => {
+    const parameters = [
+        parseInt(request.params.question),
+        parseInt(request.params.correct_ans),
+    ];
+    const query = 'SELECT * FROM project2 WHERE id = ? AND is_deleted = 0';
+    connection.query(query, parameters, (error, rows) => {
+        if (error) {
+            response.status(500);
+            response.json({
+                ok: false,
+                results: error.message,
+            });
+        } else {
+            const question = rows.map(rowToQuestion);
+            response.json({
+                ok: true,
+                results: rows.map(rowToQuestion),
+            });
+        }
+    });
+});
 
 
-connection.end();
+// connection.end(); // connection stays open as long as service is running
