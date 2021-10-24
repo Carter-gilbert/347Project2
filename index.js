ec9d56a117a994cb8ef2db8b78d9103470a8268e
Carter Gilbert <GILBE3AC@dukes.jmu.edu>
2021-10-24 17:21:25 -0400

    start

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const service = express();
+const fs = require('fs');
+const mysql = require('mysql');
+
+const json = fs.readFileSync('credentials.json', 'utf8');
+const credentials = JSON.parse(json);
+
+const connection = mysql.createConnection(credentials);
+connection.connect(error => {
+    if (error) {
+        console.error(error);
+        process.exit(1);
+    }
+});
+
+
+// const port = 8443;
+// service.listen(port, () => {
+//     console.log(`We're live on port ${port}!`);
+// });
+
+
+const selectQuery = 'SELECT * FROM memory';
+connection.query(selectQuery, (error, rows) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(rows);
+    }
+});
+
+
+
+connection.end();
